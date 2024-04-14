5b803d2df0e7f886f93297b88fc57228269e4281
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-14 21:25:47 +0200

    ability
    added ability with description and points

---
+++ phase/abilities.py
@@ -0,0 +1,26 @@
+abilities = {
+    "Damage": {
+        "points": 1,
+        "description": "Damage is needed when attacking."
+    },
+    "Defense": {
+        "points": 1,
+        "description": "Defense is calculating by defense + dexterity."
+    },
+    "Dexterity": {
+        "points": 1,
+        "description": "Dexterity is important when defending and damaging."
+    },
+    "Skill": {
+        "points": 1,
+        "description": "SKill is important when damage and when crit damage."
+    },
+    "Health": {
+        "points": 50,
+        "description": "Health can be restored after every fight."
+    },
+    "Luck": {
+        "points": 1,
+        "description": "Luck is important for crit damage."
+    }
+}
