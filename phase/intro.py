cdb75834dd44714205ba4dff199d319fef8d5cd8
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-14 13:47:56 +0200

    intro update
    added code to def

--- phase/intro.py
+++ phase/intro.py
@@ -1,18 +1,22 @@
 import game_constants
 
-while True:
 
-    print("Welcome to the game called Arcadius. Are you ready?\n0 - Quit game\n1 - Im ready")
-    intro_choice = int(input("What do you want to choose? "))
+def intro():
+    while True:
 
-    if intro_choice not in [0, 1]:
-        print("Invalid input")
-        continue
+        print("Welcome to the game called Arcadius. Are you ready?\n0 - Quit game\n1 - Im ready")
+        intro_choice = int(input("What do you want to choose? "))
 
-    if intro_choice == 0:
-        print("Goodbye")
-        break
-    elif intro_choice == 1:
-        print("Welcome in world of Arcadius")
-        break
-    print(game_constants.DIVIDER)
+        if intro_choice not in [0, 1]:
+            print("Invalid input")
+            print(game_constants.DIVIDER)
+            continue
+
+        if intro_choice == 0:
+            print("Goodbye")
+            print(game_constants.DIVIDER)
+            break
+        elif intro_choice == 1:
+            print("Welcome in world of Arcadius")
+            print(game_constants.DIVIDER)
+            break
