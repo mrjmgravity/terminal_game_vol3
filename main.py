251b1c779099eeefef41ee2d59d6e6b9d3c639ef
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-14 21:16:32 +0200

    Name and main and Dividers
    improved name.py and added to main. and added dividers and make inputs int

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from phase.intro import intro
+from phase.name import set_nickname
 
 
 intro()
+set_nickname()
--- phase/name.py
+++ phase/name.py
@@ -1,19 +1,22 @@
+from game_constants import DIVIDER
 
 
 def set_nickname():
     while True:
         name_input = input("What is your nickname? ")
         print(f"Is this your nickname? {name_input}")
-        name_choice = input("0 - Change the nickname\n1 - Confirm the nickname")
+        print("0 - Change the nickname\n1 - Confirm the nickname")
+        name_choice = int(input("Choose one: "))
 
         if name_choice not in [0, 1]:
             print("Invalid input")
+            print(DIVIDER)
             continue
 
         if name_choice == 0:
-            name_input = input("What is your nickname? ")
-            break
+            print(DIVIDER)
+            continue
         elif name_choice == 1:
             print(f"Hello {name_input}")
+            print(DIVIDER)
             break
-
