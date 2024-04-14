25cb227f035bc487dc058feb39f41f9294633492
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-14 13:43:20 +0200

    intro
    Added intro function to start the game

--- phase/intro.py
+++ phase/intro.py
@@ -1,4 +1,18 @@
 import game_constants
 
-print("Welcome to the game called Arcadius. Are you ready?\n0 - Quit game\n1 - Im ready")
-print(game_constants.DIVIDER)
+while True:
+
+    print("Welcome to the game called Arcadius. Are you ready?\n0 - Quit game\n1 - Im ready")
+    intro_choice = int(input("What do you want to choose? "))
+
+    if intro_choice not in [0, 1]:
+        print("Invalid input")
+        continue
+
+    if intro_choice == 0:
+        print("Goodbye")
+        break
+    elif intro_choice == 1:
+        print("Welcome in world of Arcadius")
+        break
+    print(game_constants.DIVIDER)
