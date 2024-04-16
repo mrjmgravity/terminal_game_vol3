b5ad6ae8a991fdf9642f4be2c4be876c21a2c968
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-16 18:21:45 +0200

    updating code
    updating code with my mentor. Deleting stuff

--- phase/abilities.py
+++ phase/abilities.py
@@ -51,22 +51,17 @@ def abilities_assign():
 
         if assign_input == 1:
             abilities["Damage"]["points"] += 1
-            first_assign -= 1
         elif assign_input == 2:
             abilities["Defense"]["points"] += 1
-            first_assign -= 1
         elif assign_input == 3:
             abilities["Dexterity"]["points"] += 1
-            first_assign -= 1
         elif assign_input == 4:
             abilities["Skill"]["points"] += 1
-            first_assign -= 1
         elif assign_input == 5:
             abilities["Health"]["points"] += 5
-            first_assign -= 1
-        elif assign_input == 6:
+        else:
             abilities["Luck"]["points"] += 1
-            first_assign -= 1
+        first_assign -= 1
 
         print("You have assigned all your points.")
         print(DIVIDER)
