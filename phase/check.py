a6b7d2e815bbcb4271e23af1ec78817134abadcd
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-04-19 21:01:35 +0200

    menu
    Added menu . but its not finished yet

--- phase/check.py
+++
@@ -1,4 +0,0 @@
-
-
-def phase_check(next_phase):
-    pass
---
+++ phase/menu.py
@@ -0,0 +1,30 @@
+import game_constants
+import phase_constants
+
+
+def main_menu(next_phase):
+    while True:
+        print("0 - Continue to FIGHT")
+        print("1 - Edit Hero")
+        print("2 - Save the game")
+        print("3 - Quit the game")
+        menu_input = int(input("What is your choice? "))
+
+        if menu_input not in [0, 1, 2, 3]:
+            print("Invalid input")
+            continue
+
+        if menu_input == 0:
+            return phase_constants.FIGHT
+        elif menu_input == 1:
+            #TODO editovanie hrdinu
+            pass
+        elif menu_input == 2:
+            #TODO ulozenie hry
+            pass
+        elif menu_input == 3:
+            print("Goodbye")
+            print(game_constants.DIVIDER)
+            return phase_constants.END
+        else:
+            print("Invalid input")
