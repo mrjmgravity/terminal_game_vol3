67e13afcc0a5ff193a757a1e3ac07c448ae3f9e8
mrjmgravity <118164654+mrjmgravity@users.noreply.github.com>
2024-05-04 21:41:47 +0200

    fight
    added fight where the fight will be simulated

---
+++ fight/fight.py
@@ -0,0 +1,12 @@
+from battle import hero_prepared
+from battle import enemy_prepared
+import random
+
+def fighting(is_alive, health_left):
+    while True:
+        print("You are attacking first\n")
+        print(f"You have attacked an enemy with damage {}")
+
+
+def damage_dealt(damage):
+    damage = randint()
